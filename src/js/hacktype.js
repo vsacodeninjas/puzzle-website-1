e7c430ccc3dcc88cc8d49976180e275050c1afae
littleblack111 <littleblack11111@gmail.com>
2023-11-03 16:39:45 +0800

    added greetings with cool hacker typing js

---
+++ src/js/hacktype.js
@@ -0,0 +1,50 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "1234567890";
+const special = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+let interval = null;
+
+function hackType() {
+const elements = document.querySelectorAll(".hacktype");
+let intervals = [];
+
+elements.forEach((element) => {
+	let iteration = 0;
+
+	intervals.push(
+	setInterval(() => {
+		element.innerText = element.innerText
+		.split("")
+		.map((letter, index) => {
+			if (index < iteration) {
+			return element.dataset.value[index];
+			}
+
+			return letters[Math.floor(Math.random() * 26)];
+		})
+		.join("");
+
+		if (iteration >= element.dataset.value.length) {
+		clearInterval(intervals.shift());
+		}
+
+		iteration += 1 / 5;
+	}, 20)
+	);
+});
+}
+
+hackType(); // Call hackType initially
+
+setInterval(() => {
+const intervals = document.querySelectorAll(".hacktype-interval");
+intervals.forEach((interval) => clearInterval(interval)); // Clear existing intervals
+hackType(); // Call hackType again
+}, 10000); // Repeat every 5 seconds
+
+// Check if the document becomes visible again
+document.addEventListener("visibilitychange", () => {
+if (!document.hidden) {
+	hackType(); // Call hackType when the user comes back from being away
+}
+});
