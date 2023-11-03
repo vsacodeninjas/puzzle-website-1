e0201e53a26ae8d51c620b38a6be8c8c928b1c77
littleblack111 <littleblack11111@gmail.com>
2023-11-03 22:14:45 +0800

    re-formmated try me card with small ones

--- src/js/randomtryme.js
+++ src/js/randomtryme.js
@@ -1,4 +1,4 @@
 window.onload = function() {
-	const correct = document.getElementById(Math.floor(Math.random() * 6) + 1 + "tryme")
+	const correct = document.getElementById(Math.floor(Math.random() * 12) + 1 + "tryme")
 	correct.href = "javascript:finishLevel(nextlv = '/lv2')";
 }
