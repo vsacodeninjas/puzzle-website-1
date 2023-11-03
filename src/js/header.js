80d8b9214fd4ebc899f766bc90984877f23d9547
littleblack111 <littleblack11111@gmail.com>
2023-11-03 16:16:19 +0800

    solve not going back when window.history is none, will redirect to /(index)

--- src/js/header.js
+++ src/js/header.js
@@ -2,16 +2,23 @@ let header = document.querySelector('[header]');
 let newElement = document.createElement('div');
 let page = header.getAttribute('page');
 
-// console.log(page);
 newElement.innerHTML = `
 <!-- header -->
 <header id="navBarHeader">
 	<nav class="navBar">
 		<div class="hleft">
-			<a onclick="window.history.back()" id="btnBack" style="text-decoration: unset; cursor: pointer;">Back<span></span></span></a>
+			<a onclick="goBack()" id="btnBack" style="text-decoration: unset; cursor: pointer;">Back<span></span></span></a>
 		</div>
 	</nav> 
 </header>
 `;
 
 header.appendChild(newElement);
+
+function goBack() {
+  if (window.history.length > 1) {
+    window.history.back();
+  } else {
+    window.location.replace('/');
+  }
+}
