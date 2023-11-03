8f2268247f568b1c01e9d54b673a38b947c15c22
littleblack111 <littleblack11111@gmail.com>
2023-11-03 20:34:42 +0800

    update language not good

--- src/js/finishlevel.js
+++ src/js/finishlevel.js
@@ -5,7 +5,7 @@ function finishLevel() {
 	<dialog style="z-index: 1000;" id="dialog">
 		<h2>Yoooo! Congratulations!!!</h2>
 		<p>You've completed the ${dialog.getAttribute('page')} challenge!</p>
-		<h4>Redirecting you to next level when presed the button!!!</h4>
+		<h4>Press the button below to go to the next level!!!</h4>
 		<form method="dialog">
 			<button autofocus id="finishPressed">Let's Go!</button>
 			<button id="finishHomePressed">Go Home!</button>
